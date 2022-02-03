0b7a30fe337821cf03ee947da8a4f401f315571b
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-04 02:30:23 +0530

    Adding UC5

---
+++ Generics/GenericMax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericMax
+    {
+        public class GenericMaximum<T> where T : IComparable //Where T data type should inherit IComparable interface
+        {
+            public T[] value;
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+
+            public T[] Sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+
+            }
+
+            void add(params int[] a)
+            {
+
+            }
+
+            public T MaxValue(params T[] values)
+            {
+                var sorted_values = Sort(values);
+
+                return sorted_values[sorted_values.Length - 1];
+            }
+
+            public T MaxMethod()
+            {
+                var max = MaxValue(this.value);
+                return max;
+            }
+
+            public void PrintMaxValue()
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine("Maximum value is " + max);
+            }
+        }
+}
