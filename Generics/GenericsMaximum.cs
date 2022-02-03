f6fe76591c926e6346282e87b4f2ca98d6b986fd
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-04 02:25:52 +0530

    Adding UC2

--- Generics/GenericsMaximum.cs
+++ Generics/GenericsMaximum.cs
@@ -27,6 +27,23 @@ namespace Generics
 
         }
 
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+
     }
     
 }
