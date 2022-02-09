82545e5106fb29c71570dcf3893f78f8a8529457
sunilpowar <96655604+sunilpowar@users.noreply.github.com>
2022-02-09 07:10:08 +0530

    Update Program.cs

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,7 +11,7 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generic demo");
-            /*
+            
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(23, 43, 65);
             Console.WriteLine(output);
@@ -24,18 +24,18 @@ namespace Generics
             Console.WriteLine(maximum1);
             Console.WriteLine(maximum2);
             Console.WriteLine(maximum3);
-            */
-            /* Console.WriteLine("Finding max Number :");
+            
+             Console.WriteLine("Finding max Number :");
              string maximum1 = MaximumNumberCheck.FindMaximum<int>(29, 76, 94);
              string maximum2 = MaximumNumberCheck.FindMaximum<float>(231.2f, 112.45f, 32.56f);
              string maximum3 = MaximumNumberCheck.FindMaximum<string>("202", "323", "441");
              Console.WriteLine(maximum1);
              Console.WriteLine(maximum2);
              Console.WriteLine(maximum3);
-            */
+            
             Console.WriteLine("Finding max Number :");
-            //Generic<int> first = new Generic<int>();
-           // first.FindMax(1, 2, 3);
+            Generic<int> first = new Generic<int>();
+            first.FindMax(1, 2, 3);
             Generic<int> first = new Generic<int>(1, 2, 3);
 
 
